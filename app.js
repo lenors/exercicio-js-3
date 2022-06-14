cd7b6bbb9298c38eaf3f2b2fea10a1230de55ac5
Leno rafael <73203800+lenors@users.noreply.github.com>
2022-06-14 13:09:44 -0300

    exercicio

---
+++ app.js
@@ -0,0 +1,16 @@
+// for (let contador = 1; contador <= 10; contador +=1)
+// {console.log(contador)}
+// let contador = 10
+// while (contador >= 1) {console.log (contador)
+//         contador -= 1;
+//   }
+// let contador = 1
+//  while (100 >= contador) {
+//    console.log (contador);
+//     contador += 2;
+//  }
+ let contador = 0
+ while (100 >= contador) {
+   console.log (contador);
+    contador += 2;
+ }
